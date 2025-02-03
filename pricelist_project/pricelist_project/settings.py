e4fc284b720315435813df2676277a07bf0eb457
iabdurahmon <abdurahmon15122006@gmail.com>
2025-02-04 00:11:55 +0500

    production

--- pricelist_project/pricelist_project/settings.py
+++ pricelist_project/pricelist_project/settings.py
@@ -130,8 +130,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = '/static/'  # добавь слеш в начале
-STATIC_ROOT = BASE_DIR / "static"
+STATIC_URL = '/static/'  # ОБЯЗАТЕЛЬНО слэш в начале!
+STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 
 # Default primary key field type
