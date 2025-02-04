143a79d39af9762542964c9407dcbebaee9b2ea1
iabdurahmon <abdurahmon15122006@gmail.com>
2025-02-04 20:22:32 +0500

    cont ton

--- pricelist_project/pricelist_project/settings.py
+++ pricelist_project/pricelist_project/settings.py
@@ -148,6 +148,8 @@ STATICFILES_DIRS = [
     BASE_DIR / "products" / "static",
 ]
 
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
